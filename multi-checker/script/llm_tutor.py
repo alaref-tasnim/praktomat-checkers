330590242e1051358a5bf7c5d409f93121c90d85
simonroeschhso <simon.roesch@hs-offenburg.de>
2026-07-23 12:41:08 +0200

    fix code duplication from merge
    Removed redundant code for extracting PDF path.

--- multi-checker/script/llm_tutor.py
+++ multi-checker/script/llm_tutor.py
@@ -83,14 +83,6 @@ def check(opts: LlmTutorOptions):
             else:
                 pdf = pjoin(exTest_dir, 'pdf', assignment.pdf)
 
-            # aufgabe pfad extrahieren
-            if assignment.pdf is None:
-                configError(f'No extra file defined for assignment {assignment.id}')
-            if opts.pdf_dir:
-                pdf = pjoin(opts.pdf_dir, assignment.pdf)
-            else:
-                pdf = pjoin(exTest_dir, 'pdf', assignment.pdf)
-
             # student Solution
             debug(f'opts.sourceDir = {opts.sourceDir}')
             nestedSourceDir = findSolutionDir(opts.sourceDir, lambda x: isDir(pjoin(x, 'src')))
